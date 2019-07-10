a02729c3011cc43a4be9bee7b90a790ba15ac8bf
lilyhoratio <lilyhzhou@gmail.com>
2019-07-10 15:33:17 -0700

    finished building function to create articles

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -113,11 +113,24 @@ const data = [
 
 */
 
-const articleAccordion = document.querySelector(".articles")
+const articleAccordion = document.querySelector(".articles");
 
-data.forEach(article => articleAccordion.appendChild(createArticle(article.title, article.date, article.firstParagraph, article.secondParagraph, article.thirdParagraph)))
+data.forEach(article =>
+  articleAccordion.appendChild(
+    createArticle(
+      article.title,
+      article.date,
+      article.firstParagraph,
+      article.secondParagraph,
+      article.thirdParagraph
+    )
+  )
+);
+
+// articleAccordion.appendChild(createArticle(data));
 
 function createArticle(title, date, p1, p2, p3) {
+// function createArticle(data) {
   // elements
   const article = document.createElement("div");
   const articleTitle = document.createElement("h2");
@@ -134,7 +147,29 @@ function createArticle(title, date, p1, p2, p3) {
   article.appendChild(expandButton);
 
   // apply classes
+  article.classList.add("article");
+  articleDate.classList.add("date");
+  expandButton.classList.add("expandButton");
 
-  article.classList.add("article")
-  articleDate.classList.add("date")
+  //text
+
+  articleTitle.textContent = title;
+  articleDate.textContent = date;
+  firstPara.textContent = p1;
+  secondPara.textContent = p2;
+  thirdPara.textContent = p3;
+
+  // articleTitle.textContent = `${data.title}`;
+  // articleDate.textContent = `${data.date}`;
+  // firstPara.textContent = `${data.firstParagraph}`;
+  // secondPara.textContent = `${data.secondParagraph}`;
+  // thirdPara.textContent = `${data.thirdParagraph}`;
+  expandButton.textContent = "Expand";
+
+  expandButton.addEventListener("click", () => {
+    article.classList.toggle("article-open")
+  })
+
+  return article;
 }
+
